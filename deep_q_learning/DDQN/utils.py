0f6cb33c872f063551bbe0b132bb027622d9bc4a
Shilu <shilu842003@gmail.com>
2023-03-15 19:55:24 +0530

    added dqn variants

---
+++ deep_q_learning/DDQN/utils.py
@@ -0,0 +1,62 @@
+import collections
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import gymnasium as gym
+import tensorflow as tf
+from gymnasium.wrappers import *
+
+
+def manage_memory():
+    gpus = tf.config.list_physical_devices('GPU')
+    if gpus:
+        try:
+            for gpu in gpus:
+                tf.config.experimental.set_memory_growth(gpu, True)
+        except RuntimeError as e:
+            print(e)
+
+
+def plot_learning_curve(scores, epsilons, filename, lines=None):
+    x = [_ for _ in range(len(scores))]
+    fig=plt.figure()
+    ax=fig.add_subplot(111, label="1")
+    ax2=fig.add_subplot(111, label="2", frame_on=False)
+
+    ax.plot(x, epsilons, color="C0")
+    ax.set_xlabel("Training Steps", color="C0")
+    ax.set_ylabel("Epsilon", color="C0")
+    ax.tick_params(axis='x', colors="C0")
+    ax.tick_params(axis='y', colors="C0")
+
+    N = len(scores)
+    running_avg = np.empty(N)
+    for t in range(N):
+	    running_avg[t] = np.mean(scores[max(0, t-20):(t+1)])
+
+    ax2.scatter(x, running_avg, color="C1")
+    ax2.axes.get_xaxis().set_visible(False)
+    ax2.yaxis.tick_right()
+    ax2.set_ylabel('Score', color="C1")
+    ax2.yaxis.set_label_position('right')
+    ax2.tick_params(axis='y', colors="C1")
+
+    if lines is not None:
+        for line in lines:
+            plt.axvline(x=line)
+
+    plt.savefig(filename)
+
+
+def make_env(env_name, video_file_name, episode_freq_fo_video): 
+    env = gym.make(env_name, render_mode="rgb_array")
+    
+    if len(env.observation_space.shape) >= 3: 
+        print("yes")
+        #env = AtariPreprocessing(env, 10, 4, 84, False, True)
+        env = ResizeObservation(env, 84)
+        env = GrayScaleObservation(env, keep_dim=False)
+        env = FrameStack(env, 4, lz4_compress=False)
+        env = NormalizeObservation(env)
+
+    return env

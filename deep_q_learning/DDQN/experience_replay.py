19ddd6a92f339351cdc9f5690212c49bac5425d7
Shilu <shilu842003@gmail.com>
2023-03-15 19:56:57 +0530

    added dqn's variants

---
+++ deep_q_learning/DDQN/experience_replay.py
@@ -0,0 +1,37 @@
+import numpy as np 
+
+class ExperienceReplayBuffer: 
+    def __init__(self, max_memory, input_shape, n_actions): 
+        self.mem_size = max_memory
+        self.mem_counter = 0
+        self.state_memory = np.zeros((self.mem_size, *input_shape),
+                                     dtype=np.float32)
+        self.new_state_memory = np.zeros((self.mem_size, *input_shape),
+                                         dtype=np.float32)
+
+        self.action_memory = np.zeros(self.mem_size, dtype=np.int64)
+        self.reward_memory = np.zeros(self.mem_size, dtype=np.float32)
+        self.terminal_memory = np.zeros(self.mem_size, dtype=bool)
+
+    def store_experience(self, state, action, reward, next_state, done): 
+        index = self.mem_counter % self.mem_size 
+
+        self.state_memory[index] = state
+        self.new_state_memory[index] = next_state
+        self.reward_memory[index] = reward
+        self.action_memory[index] = action
+        self.terminal_memory[index] = done
+        self.mem_counter += 1
+        
+
+    def sample_experience(self, batch_size):
+        max_mem = min(self.mem_counter, self.mem_size)
+        batch_index = np.random.choice(max_mem, batch_size, replace=False)
+
+        states = self.state_memory[batch_index]
+        next_states = self.new_state_memory[batch_index]
+        rewards = self.reward_memory[batch_index]
+        actions = self.action_memory[batch_index]
+        terminal = self.terminal_memory[batch_index]
+
+        return states, actions, rewards, next_states, terminal
---
+++ deep_q_learning/DDQN/lunarlander.py
@@ -0,0 +1,70 @@
+import gymnasium as gym
+import time
+import signal
+import time
+import sys
+import pickle
+
+env = make_env("LunarLander-v2", "videos/", 50)
+action_space = [_ for _ in range(env.action_space.n)]
+record = True
+
+
+trainer_params = {
+    "noe": 650, 
+    "max_steps": 10000,
+    "max_eps": 1,
+    "min_eps": 0.1,
+    "eps_decay_rate": 1e-4,
+    "eps": 1,
+    "action_space": action_space,
+    "is_tg": True,
+    "tg_bot_freq_epi": 10,
+    "record": record,
+    "gamma": 0.99, 
+    "lr": 0.0001, 
+    "input_dims": env.observation_space.shape,
+    "mem_size" : 100000,
+    "batch_size" : 32,
+    "replace" : 150,
+    "algo" : "DDQN",
+    "env_name" : "lunarlander",
+    "n_actions" : len(action_space),
+    "chkpt_dir": "tmp/ddqn/",
+    "actions": action_space,
+    "target_score": 200,
+    "tau": 0.1,
+    "soft_update": True,
+    "video_prefix": "ddqn",
+    "checkpoint": False
+}
+
+    
+if __name__ == "__main__": 
+    
+    try: 
+        manage_memory()
+       
+        trainer = Trainer(env, trainer_params)
+        episode_rewards, epsilon_history, avg_rewards, best_reward = trainer.train_rl_model()
+        
+        with open("ddqn_episode_rewards.obj", "wb") as f: 
+            pickle.dump(episode_rewards, f)
+        
+        with open("ddqn_epsilon_history.obj", "wb") as f: 
+            pickle.dump(epsilon_history, f)
+        
+        with open("ddqn_avg_rewards.obj", "wb") as f: 
+            pickle.dump(avg_rewards, f)
+            
+        plot_learning_curve(episode_rewards, epsilon_history, "vanilla_ddqn")
+        
+        model_path = "/kaggle/working/tmp/ddqn/lunarlander_DDQN_q_value/"
+
+        eval = Eval(env, model_path, action_space, 1)
+
+        rewards = eval.test()
+
+    except Exception as error: 
+        raise error
+        

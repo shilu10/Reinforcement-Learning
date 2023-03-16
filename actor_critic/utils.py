f345fbe0a27a7f13fc23a4908e5634f6ca93144d
Shilu <shilu842003@gmail.com>
2023-03-16 07:56:05 +0530

    added actor critic

---
+++ actor_critic/utils.py
@@ -0,0 +1,41 @@
+import collections
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import gymnasium as gym
+import tensorflow as tf
+from gymnasium.wrappers import *
+
+
+def manage_memory():
+    gpus = tf.config.list_physical_devices('GPU')
+    if gpus:
+        try:
+            for gpu in gpus:
+                tf.config.experimental.set_memory_growth(gpu, True)
+        except RuntimeError as e:
+            print(e)
+
+
+def plot_learning_curve(scores, figure_file):
+
+    x = [_ for _ in range(len(scores))]
+    running_avg = np.zeros(len(scores))
+    for i in range(len(running_avg)):
+        running_avg[i] = np.mean(scores[max(0, i-100):(i+1)])
+    plt.plot(x, running_avg)
+    plt.title('Running average of previous 100 scores')
+    plt.savefig(figure_file)
+
+
+def make_env(env_name): 
+    env = gym.make(env_name, render_mode="rgb_array")
+    
+    if len(env.observation_space.shape) >= 3: 
+        #env = AtariPreprocessing(env, 10, 4, 84, False, True)
+        env = ResizeObservation(env, 84)
+        env = GrayScaleObservation(env, keep_dim=False)
+        env = FrameStack(env, 4, lz4_compress=False)
+        env = NormalizeObservation(env)
+
+    return env

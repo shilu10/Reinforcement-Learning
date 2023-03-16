b356953d6f51dc062a09a094f31d09a651fcee68
adminuser <adminuser@kali.mvm>
2023-03-16 07:56:05 +0530

    added actor critic

---
+++ actor_critic/video_recorder.py
@@ -0,0 +1,19 @@
+import numpy as np 
+import imageio
+
+
+class RecordVideo: 
+    
+    def __init__(self, prefix_fname,  out_directory="videos/", fps=10): 
+        self.prefix_fname = prefix_fname
+        self.out_directory = out_directory
+        self.fps = fps
+        self.images = []
+        
+    def add_image(self, image): 
+        self.images.append(image)
+    
+    def save(self, episode_no): 
+        name = self.out_directory + self.prefix_fname + "_" + str(episode_no)
+        imageio.mimsave(name, [np.array(img) for i, img in enumerate(self.images)], fps=self.fps)
+        self.images = []

e4b6d54de1d8500e90d17e5a0c16014760242b95
Shilu <shilu842003@gmail.com>
2023-03-22 14:25:27 +0530

    added dueling dqn

---
+++ Dueling_DQN/train.py
@@ -0,0 +1,115 @@
+ifrom npy_append_array import NpyAppendArray
+import numpy as np
+
+class Trainer:   
+    def __init__(self, env, trainer_params): 
+       
+        self.env = env 
+        self.noe = trainer_params.get("noe")
+        self.max_steps = trainer_params.get("max_steps")
+       
+        self.eps_decay_rate = trainer_params.get("eps_decay_rate")
+        self.action_space = trainer_params.get("action_space")
+        self.is_tg = trainer_params.get("is_tg")
+        self.tg_bot_freq_epi = trainer_params.get("tg_bot_freq_epi")
+        self.record = trainer_params.get("record")
+        self.agent_params = {
+                        "gamma":  trainer_params.get("gamma"), 
+                        "lr":  trainer_params.get("lr"), 
+                        "input_dims":  trainer_params.get("input_dims"),
+                        "mem_size" :  trainer_params.get("mem_size"),
+                        "batch_size" :  trainer_params.get("batch_size"),
+                        "replace" :  trainer_params.get("replace"),
+                        "algo" :  trainer_params.get("algo"),
+                        "env_name" :  trainer_params.get("env_name"),
+                        "n_actions" :  trainer_params.get("n_actions"),
+                        "chkpt_dir":  trainer_params.get("chkpt_dir"),
+                        "actions":  trainer_params.get("actions"),
+                        "eps": trainer_params.get("eps"),
+                        "min_eps": trainer_params.get("min_eps"),
+                        "eps_decay_rate": trainer_params.get("eps_decay_rate"),
+                        
+                    }
+        
+        self.agent = Agent(self.agent_params)
+        self.checkpoint = trainer_params.get("checkpoint")
+        
+        self.writer = Writer("model_training_results.txt")
+        self.recorder = RecordVideo(trainer_params.get("video_prefix"), "videos/", 20)
+        
+        self.target_score = trainer_params.get("target_score")
+
+    def train_rl_model(self): 
+        episode_rewards = []
+        epsilon_history = []
+        avg_rewards = []
+        best_reward = float("-inf")
+
+        if self.checkpoint:
+          self.agent.load_models()
+
+        for episode in range(self.noe): 
+            n_steps = 0 
+            episodic_loss = 0
+            state = self.env.reset()
+            reward = 0 
+            
+            if self.record and episode%100==0: 
+                img = self.env.render()
+                self.recorder.add_image(img)
+
+            for step in range(self.max_steps): 
+                
+                if self.record and episode%100==0: 
+                    img = self.env.render()
+                    self.recorder.add_image(img)
+
+                if type(state) == tuple: 
+                    state = state[0]
+                state = state
+
+                action = self.agent.choose_action(state)
+
+                next_info = self.env.step(action)
+                next_state, reward_prob, terminated, truncated, _ = next_info
+                done = truncated or terminated
+                reward += reward_prob
+
+                self.agent.store_experience(state, action, reward_prob, next_state, done)
+                eps = self.agent.learn()
+
+                state = next_state
+                n_steps += 1 
+               
+                
+                if done: 
+                    break
+
+            epsilon_history.append(eps)
+            episode_rewards.append(reward)
+            avg_reward = np.mean(episode_rewards[-100:])
+            avg_rewards.append(avg_reward)
+
+            result = f"Episode: {episode}, Epsilon: {eps}, Steps: {n_steps}, Reward: {reward}, Best reward: {best_reward}, Avg reward: {avg_reward}"
+            self.writer.write_to_file(result)
+            print(result)
+            
+            # Saving Best Model
+            if reward > best_reward: 
+                best_reward = reward
+                self.agent.save_models()
+            
+            # video Recorder
+            if episode % 100 ==0:
+                self.recorder.save(episode)
+                
+          # Telegram bot
+            if self.is_tg and episode % self.tg_bot_freq_epi == 0: 
+                info_msg(episode+1, self.noe, reward, best_reward, "d")
+                
+         # Eatly Stopping
+            if episode > 100 and np.mean(episode_rewards[-100:]) >= self.target_score: 
+                break
+                
+                
+        return episode_rewards, epsilon_history, avg_rewards, best_reward

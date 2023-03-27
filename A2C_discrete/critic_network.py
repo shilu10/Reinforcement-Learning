0efe6a6b91374a2786026de594c26a2c2a23b581
Shilu <shilu842003@gmail.com>
2023-03-27 12:39:17 +0530

    updated actor critic

---
+++ A2C_discrete/critic_network.py
@@ -0,0 +1,13 @@
+lass CriticNetwork(tf.keras.Model):
+    def __init__(self, input_dims, action_dim=1):
+        super(CriticNetwork, self).__init__()
+        self.fc1 = Dense(512, activation="relu", input_shape=input_dims, kernel_initializer="he_uniform")
+        self.fc2 = Dense(256, activation="relu", kernel_initializer="he_uniform")
+        self.fc2 = Dense(64, activation="relu", kernel_initializer="he_uniform")
+        self.fc3 = Dense(1)
+
+    def call(self, x):
+        x = self.fc1(x)
+        x = self.fc2(x)
+        x = self.fc3(x)
+        return x
